977e10bd44e7a2da3ef4ce3893b34e05012bdf8b
John Barksdale <jdbarks@gmail.com>
2017-11-07 00:20:26 -0600

    All tests passing

--- ASCII_Art/ascii_art.py
+++ ASCII_Art/ascii_art.py
@@ -16,11 +16,21 @@ import string
 
 # print("answer")
 
-def ascii_art(length, height, word_to_asciify, ASCIIbet):
-    letter_to_number = string.lowercase
 
-    print(letter_to_number)
-    return ['### ', "#   ", '##  ', '#   ', '### ']
+def ascii_art(length, height, word_to_asciify, ASCIIbet):
+    asciified_text = []
+
+    num_word = word_to_numbers(word_to_asciify)
+    for row in range(height):
+        line =''
+        for number in num_word:
+            ASCII_letter_low = number * length
+            ASCII_letter_high = (number * length) + length
+            line += ASCIIbet[row][ASCII_letter_low:ASCII_letter_high]
+
+        asciified_text.append(line)
+
+    return asciified_text
 
 
 def word_to_numbers(word):
@@ -35,3 +45,14 @@ def word_to_numbers(word):
 
     return num_word
 
+def print_ASCII_art(length, height, word_to_asciify, ASCIIbet):
+    for item in ascii_art(length, height, word_to_asciify, ASCIIbet):
+        print(item)
+
+if __name__ == "__main__":
+    print_ASCII_art(4, 5, 'ASCIIART', ASCIIbet = [' #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ',
+                    '# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ',
+                    '### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ',
+                    '# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ',
+                    '# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  ']
+)
