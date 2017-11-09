d2e3dcdef4943a424e802eb7cd80df95800ad236
John Barksdale <jdbarks@gmail.com>
2017-11-08 23:34:53 -0600

    added tests for binary_grouping and tests are passing

--- Chuck_Norris/chuck_norris.py
+++ Chuck_Norris/chuck_norris.py
@@ -2,12 +2,36 @@ import binascii
 
 
 def message_to_binary(message):
+    # Truns each character into a binary string that is 7 digits long and
+    # returns them as one large string
     binary = ''
     for letter in message:
         binary += format(int.from_bytes(letter.encode(), 'big'), '#09b')[2:]
 
     return binary
 
-def chuck_norris(message):
 
-    return '0 0 00 0000 0 00'
+def binary_grouping(binary):
+    value = binary[0]
+    current_group = ''
+    binary_group = []
+    while len(binary) > 0:
+        element = binary.pop(0)
+        if element == value:
+            current_group += element
+        else:
+            binary_group.append(current_group)
+            current_group = element
+            value = element
+        if len(binary) == 0:
+                binary_group.append(current_group)
+
+    return binary_group
+
+
+def chuck_norris(message):
+    binary = list(message_to_binary(message))
+
+    grouped = binary_grouping(binary)
+
+    return grouped
--- Chuck_Norris/tests/test_chuck_norris.py
+++ Chuck_Norris/tests/test_chuck_norris.py
@@ -1,6 +1,6 @@
 import unittest
 
-from Chuck_Norris.chuck_norris import chuck_norris, message_to_binary
+from Chuck_Norris.chuck_norris import chuck_norris, message_to_binary, binary_grouping
 
 
 class TestMessageToBinary(unittest.TestCase):
@@ -20,15 +20,47 @@ class TestMessageToBinary(unittest.TestCase):
 
         self.assertEqual(binary, '0100101')
 
+
+class TestBinaryGrouping(unittest.TestCase):
+    def test_binary_grouping_c(self):
+        binary = list('1000011')
+
+        grouped = binary_grouping(binary)
+
+        self.assertEqual(grouped, ['1', '0000', '11'])
+
+    def test_binary_grouping_cc(self):
+        binary = list('10000111000011')
+
+        grouped = binary_grouping(binary)
+
+        self.assertEqual(grouped, ['1', '0000', '111', '0000', '11'])
+
+    def test_binary_grouping_percent_sign(self):
+        binary = list('0100101')
+
+        grouped = binary_grouping(binary)
+
+        self.assertEqual(grouped, ['0', '1', '00', '1', '0', '1'])
+
+    def test_binary_grouping_alternating(self):
+        binary = list('0101010101010101')
+
+        grouped = binary_grouping(binary)
+
+        self.assertEqual(grouped, ['0', '1', '0', '1', '0', '1', '0', '1',
+                                   '0', '1', '0', '1', '0', '1', '0', '1'])
+
+
 class TestChuckNorris(unittest.TestCase):
 
     def test_chuck_norris_c(self):
-        unary = chuck_norris('c')
+        unary = chuck_norris('C')
 
         self.assertEqual(unary, '0 0 00 0000 0 00')
 
     def test_chuck_norris_cc(self):
-        unary = chuck_norris('cc')
+        unary = chuck_norris('CC')
 
         self.assertEqual(unary, '0 0 00 0000 0 000 0 00 0000 0 000 00 0000 0 00')
 
